0875dbbf507477c6f79a5fe167139043eff08780
wa38373 <139689759+wa38373@users.noreply.github.com>
2024-01-24 15:57:01 +0700

    Add files via upload

---
+++ test python.py
@@ -0,0 +1,12 @@
+#for i in range(1, 17, 2):
+  #print(i)
+
+#for i in range(2, 20, 3):
+  #print(i)
+
+#for i in range (30,-40,-10):
+    #print(i)
+
+#for i in range (15,63,8):
+    #print(i)
+#จุ๊บๆ
