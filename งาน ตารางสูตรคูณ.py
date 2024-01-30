35caf9824289a83ed63b02422a3846fb06a309b4
wa38373 <139689759+wa38373@users.noreply.github.com>
2024-01-30 17:00:23 +0700

    Add files via upload

--- งาน ตารางสูตรคูณ.py
+++ งาน ตารางสูตรคูณ.py
@@ -1,6 +1,6 @@
 def print_times_table_for_five():
     print("Times Table for 5:\n")
-    for j in range(1, 11):  # Loop from 1 to 10 for the second number
+    for j in range(1, 13):  # Loop from 1 to 12 for the second number
         product = 5 * j
         print(f"5 x {j} = {product}")
 
